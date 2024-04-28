bb63d0440fa0d6ac55c6a32cd062496302863f2d
rofiatmuritala <rofiatmuritalaa@gmail.com>
2024-04-28 17:58:44 +0000

    Use Link instead a tag for how it works

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,13 +26,13 @@ export const Navbar = () => {
             >
               Benefits
             </Link>
-            <a
+            <Link
               aria-current="page"
               class="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
-              href="/howitworks"
+              to={"/howitworks"}
             >
               How it works
-            </a>
+            </Link>
 
             <Link
               class="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
